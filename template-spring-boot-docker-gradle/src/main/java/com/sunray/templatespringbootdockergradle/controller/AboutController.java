1bd397ff3bb4737632f835f74d982f65162cb3aa
Roy Luo <luorihui@hotmail.com>
2019-05-03 10:44:23 -0700

    Add spring profiles support

--- template-spring-boot-docker-gradle/src/main/java/com/sunray/templatespringbootdockergradle/controller/AboutController.java
+++ template-spring-boot-docker-gradle/src/main/java/com/sunray/templatespringbootdockergradle/controller/AboutController.java
@@ -2,17 +2,20 @@ package com.sunray.templatespringbootdockergradle.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AboutController {
+	@Value("${app.message.about}")
+	private String messageAbout;
 	public static final Logger logger = LoggerFactory.getLogger(AboutController.class.getName());
 	  @GetMapping(value = "/api/about")
 	  @CrossOrigin(origins = "*") 
 	  public String about() {
-		  final String aboutMessage =  "Sunray Version 1.0";
+		  final String aboutMessage =  "Version 1.0 " + messageAbout;
 		  logger.info(aboutMessage);
 		  return aboutMessage;
 	  }
